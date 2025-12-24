0f750f5e3aa9d0ff5d1f0fea8e217f7289678fbe
Shanmukha Rao <shrao@rippling.com>
2025-12-24 18:28:22 +0530

    Initial Commit

---
+++ MeetingAlert/MeetingAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingAlertApp.swift
+//  MeetingAlert
+//
+//  Created by Shanmukha Padala on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MeetingAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
